4fa67c54d6ec44c25cffd23fda518b3495c8f822
aiaxmaior <joshi.arjun.k@gmail.com>
2024-12-09 00:02:25 -0500

    Push3
    Midnight Push.
    Anthro finished, indexes complete.
    Consolidated marine env to new notebook
    Main notebook needs to be renamed

---
+++ Sprint3/scripts/DataDictionary.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+
+measurements= {'SDI':'Shannon Diversity Index',
+               'WQI' : 'Water Quality Index',
+               'TBNI': 'Tampa Bay Nekton Index',
+               'TBBI':  'Tampa Bay Benthic Index',
+                                           
+              }
+organizations= {'TBEP':'Tampa Bay Estuary Program',
+                'FWC': 'Florida Fish and Wildflife Commission',
+                'NOAA':  'National Oceanic and Atmospheric',
+                'NMFS':  'National Marine Fisheries Service',
+                'HC':    'Hillsborough County',
+                'HCPA':  'Hillsborough COunty Property'
+
+    
+}
+locations={
+    'HB': 'Hillsborough Bay',
+    'MTB': 'Middle Tampa Bay',
+    'OTB': 'Old Tampa Bay',
+    'LTB': 'Lower Tampa Bay'
+
+}
+compounds={
+    'Nit': 'Nitrates',
+    'Phos': 'Phosphate'
+}
+
+dic={
+    'measurement': measurements,
+    'organization': organizations,
+    'location': locations
+}
+dic_df=pd.DataFrame()
+dic_df=pd.DataFrame(pd.concat([pd.Series(measurements)
+                               ,pd.Series(locations)
+                               ,pd.Series(compounds)
+                               ,pd.Series(organizations)
+                              ]))
